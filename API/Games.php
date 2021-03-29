2c139ac05811b8f81dc780b3a49dd5ad496bfac9
RIk <Rikdenbreejen@hotmail.nl>
2021-03-29 16:56:04 +0200

    [Change] integrate Update CRUD into games.php

--- API/Games.php
+++ API/Games.php
@@ -4,6 +4,7 @@ $Models = "$Objects/Models";
 
 include_once "$Models/API/ApiResponse.php";
 include_once "$Models/Controllers/GameController.php";
+include_once "$Models/Controllers/UpdateController.php";
 include_once "$Objects/URLParameter.php";
 
 function GetSingleGame($GameID)
@@ -65,6 +66,45 @@ function DeleteGame($GameID) {
     }
 }
 
+function GetSingleUpdate($GameID, $UpdateID) {
+
+}
+function GetAllUpdates($GameID) {
+
+}
+function CreateNewUpdate($GameID, $CreateData) {
+
+}
+function ChangeUpdate($UpdateData) {
+    $GameObject = new UpdateController();
+    $Response = $GameObject->Update($UpdateData);
+    $ValidationResult = ApiResponse::GenerateResponse($Response);
+
+    $ApiResponse = new ApiResponse($ValidationResult["Result"], $ValidationResult["Message"], $Response);
+    $ApiResponse->EchoResponse();
+}
+function DeleteUpdate($GameID, $UpdateID) {
+    if (isset($GameID) && isset($UpdateID)) {
+        $GameObject = new UpdateController();
+
+        $Response = $GameObject->DeleteGameUpdate($GameID, $UpdateID);
+        if ($Response != null){
+            $ValidationResult = ApiResponse::GenerateResponse($Response);
+            $ApiResponse = new ApiResponse($ValidationResult["Result"], $ValidationResult["Message"], $Response);
+            $ApiResponse->EchoResponse();
+        }
+        else {
+            $ValidationResult = ApiResponse::GenerateResponse(false);
+            $ApiResponse = new ApiResponse($ValidationResult["Result"], $ValidationResult["Message"], false);
+            $ApiResponse->EchoResponse();
+        }
+    }
+    else{
+        $ApiResponse = new ApiResponse(false, "Undefined Variable", "");
+        $ApiResponse->EchoResponse();
+    }
+}
+
 //URL parameters
 $GamesActionValue = URLParameter::getParam("Games.php");
 if (isset($GamesActionValue) && $GamesActionValue != null) {
@@ -103,6 +143,69 @@ if (isset($GamesActionValue) && $GamesActionValue != null) {
             elseif ($ActionValue == "Delete") {
                 //Delete the game from the page
                 DeleteGame($GamesActionValue);
+            }
+            elseif ($ActionValue == "Updates") {
+                //Game Updates
+                $UpdateAction = URLParameter::getParam("Games.php/$GamesActionValue/Updates");
+                if (isset($UpdateAction) && $UpdateAction != null) {
+                    if (is_numeric($UpdateAction)) {
+                        //If Updates is a number aka an index value
+                        $UpdateActionValue = URLParameter::getParam("Games.php/$GamesActionValue/Updates/$UpdateAction");
+                        if ($UpdateActionValue != null) {
+                            if ($UpdateActionValue == "Update") {
+                                $UpdateArray = array(
+                                    "GameID" =>  $GamesActionValue,
+                                    "UpdateID" => $UpdateAction
+                                );
+                                if (array_key_exists("Name", $_POST)) {
+                                    $UpdateArray["Name"] = $_POST["Name"];
+                                }
+                                if (array_key_exists("Description", $_POST)) {
+                                    $UpdateArray["Description"] = $_POST["Description"];
+                                }
+                                if (array_key_exists("ReleaseDate", $_POST)) {
+                                    $UpdateArray["ReleaseDate"] = $_POST["ReleaseDate"];
+                                }
+                                if (array_key_exists("Visible", $_POST)) {
+                                    $UpdateArray["Visible"] = $_POST["Visible"];
+                                }
+                                ChangeUpdate($UpdateArray);
+                            }
+                            elseif ($UpdateActionValue == "Delete") {
+                                DeleteUpdate($GamesActionValue, $UpdateAction);
+                            }
+                        }
+                        else {
+                            GetSingleUpdate($GamesActionValue, $UpdateAction);
+                        }
+                    }
+                    elseif ($UpdateAction == "Create") {
+                        $CreateArray = array();
+                        if (array_key_exists("GameID", $_POST)) {
+                            $CreateArray["GameID"] = $_POST["GameID"];
+                        }
+                        if (array_key_exists("Name", $_POST)) {
+                            $CreateArray["Name"] = $_POST["Name"];
+                        }
+                        if (array_key_exists("Description", $_POST)) {
+                            $CreateArray["Description"] = $_POST["Description"];
+                        }
+                        if (array_key_exists("ReleaseDate", $_POST)) {
+                            $CreateArray["ReleaseDate"] = $_POST["ReleaseDate"];
+                        }
+                        if (array_key_exists("Visible", $_POST)) {
+                            $CreateArray["Visible"] = $_POST["Visible"];
+                        }
+                        if (array_key_exists("WebsiteAdminID", $_POST)) {
+                            $CreateArray["WebsiteAdminID"] = $_POST["WebsiteAdminID"];
+                        }
+                        CreateNewUpdate($GamesActionValue, $CreateArray);
+                    }
+                }
+                else {
+                    //Get all updates from game
+                    GetAllUpdates($GamesActionValue);
+                }
             }
         }
         else {
--- Objects/Models/Controllers/UpdateController.php
+++ Objects/Models/Controllers/UpdateController.php
@@ -1,6 +1,5 @@
 <?php
-include_once $_SERVER['DOCUMENT_ROOT']."/Database/DatabaseHandler.php";
-include_once "ObjectController.php";
+include_once $_SERVER['DOCUMENT_ROOT'] . "/Database/DatabaseHandler.php";
 
 class UpdateController extends ObjectController
 {
@@ -32,6 +31,26 @@ class UpdateController extends ObjectController
         return $this->DatabaseHandler->ExecuteQuery($Query);
     }
 
+    public function GetAllByGame($ID)
+    {
+        $ID = $this->DatabaseHandler->EscapeString($ID);
+        $Query = "SELECT 
+                updt.Name,
+                updt.Description,
+                updt.GameID,
+                updt.ReleaseDate,
+                updt.WebsiteAdminID
+            FROM
+                $this->table updt
+            ORDER BY 
+                updt.ReleaseDate DESC
+            WHERE
+                updt.GameID = $ID
+        ";
+
+        return $this->DatabaseHandler->ExecuteQuery($Query);
+    }
+
     public function GetSingle($ID)
     {
         $ID = $this->DatabaseHandler->EscapeString($ID);
@@ -53,22 +72,23 @@ class UpdateController extends ObjectController
 
     public function Create($CreateData)
     {
-        $ValuesString = "";
-        $NamesString = "";
-        $CreateDataKeys = array_keys($CreateData);
-        for ($i = 0; count($CreateData) > $i; $i++) {
-            $Key = $CreateDataKeys[$i];
-            $UpdateData[$Key] = $this->DatabaseHandler->EscapeString($CreateData[$Key]);
+        if (array_key_exists("GameID", $CreateData) && array_key_exists("Name", $CreateData)) {
+            $ValuesString = "";
+            $NamesString = "";
+            $CreateDataKeys = array_keys($CreateData);
+            for ($i = 0; count($CreateData) > $i; $i++) {
+                $Key = $CreateDataKeys[$i];
+                $UpdateData[$Key] = $this->DatabaseHandler->EscapeString($CreateData[$Key]);
 
-            if ($i != 0) {
-                $ValuesString = $ValuesString.",";
-                $NamesString = $NamesString.",";
+                if ($i != 0) {
+                    $ValuesString = $ValuesString . ",";
+                    $NamesString = $NamesString . ",";
+                }
+                $ValuesString = $ValuesString . "$UpdateData[$Key]";
+                $NamesString = $NamesString . "$Key";
             }
-            $ValuesString = $ValuesString."$UpdateData[$Key]";
-            $NamesString = $NamesString."$Key";
-        }
 
-        $Query = "INSERT 
+            $Query = "INSERT 
                 INTO $this->table 
                 (
                     $NamesString
@@ -77,34 +97,45 @@ class UpdateController extends ObjectController
                     $ValuesString
                 )
             ";
-        echo $Query;
-        return $this->DatabaseHandler->ExecuteQuery($Query);
+            echo $Query;
+            return $this->DatabaseHandler->ExecuteQuery($Query);
+        }
+        else {
+            return false;
+        }
     }
 
     public function Update($UpdateData)
     {
-        $UpdateString = "";
-        $ID = $UpdateData['ID'];
-        unset($UpdateData['ID']);
-        $UpdateDataKeys = array_keys($UpdateData);
-        for ($i = 0; count($UpdateData) > $i; $i++) {
-            $Key = $UpdateDataKeys[$i];
-            $UpdateData[$Key] = $this->DatabaseHandler->EscapeString($UpdateData[$Key]);
+        if (array_key_exists("GameID", $UpdateData) && array_key_exists("UpdateID", $UpdateData)) {
+            $UpdateString = "";
+            $ID = $UpdateData['GameID'];
+            $UpdateID = $UpdateData['UpdateID'];
+            unset($UpdateData['GameID']);
+            unset($UpdateData['UpdateID']);
+            $UpdateDataKeys = array_keys($UpdateData);
+            for ($i = 0; count($UpdateData) > $i; $i++) {
+                $Key = $UpdateDataKeys[$i];
+                $UpdateData[$Key] = $this->DatabaseHandler->EscapeString($UpdateData[$Key]);
 
-            if ($i != 0) {
-                $UpdateString = $UpdateString.",";
+                if ($i != 0) {
+                    $UpdateString = $UpdateString . ",";
+                }
+                $UpdateString = $UpdateString . "$Key = $UpdateData[$Key]";
             }
-            $UpdateString = $UpdateString."$Key = $UpdateData[$Key]";
-        }
-        
 
-        $Query = "UPDATE $this->table 
-                SET
-                $UpdateString
-                WHERE
-                    ID = $ID
-            ";
-        return $this->DatabaseHandler->ExecuteQuery($Query);
+
+            $Query = "UPDATE $this->table 
+                    SET
+                    $UpdateString
+                    WHERE
+                        GameID = $ID AND
+                        UpdateID = $UpdateID
+                ";
+            return $this->DatabaseHandler->ExecuteQuery($Query);
+        } else {
+            return false;
+        }
     }
 
     public function Delete($ID)
@@ -113,5 +144,17 @@ class UpdateController extends ObjectController
         $Query = "DELETE FROM $this->table WHERE ID = $ID";
         return $this->DatabaseHandler->ExecuteQuery($Query);
     }
+    public function DeleteGame($GameID)
+    {
+        $GameID = $this->DatabaseHandler->EscapeString($GameID);
+        $Query = "DELETE FROM $this->table WHERE GameID = $GameID";
+        return $this->DatabaseHandler->ExecuteQuery($Query);
+    }
+    public function DeleteGameUpdate($GameID, $UpdateID)
+    {
+        $GameID = $this->DatabaseHandler->EscapeString($GameID);
+        $UpdateID = $this->DatabaseHandler->EscapeString($UpdateID);
+        $Query = "DELETE FROM $this->table WHERE GameID = $GameID AND UpdateID = $UpdateID";
+        return $this->DatabaseHandler->ExecuteQuery($Query);
+    }
 }
-?>

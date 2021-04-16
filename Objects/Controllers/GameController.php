ccef1dd84a0adc2b1f4e4267931653fc42fba194
RIk <Rikdenbreejen@hotmail.nl>
2021-04-16 16:28:09 +0200

    [Addition] Setup game controller

--- Objects/Controllers/GameController.php
+++ Objects/Controllers/GameController.php
@@ -1,5 +1,28 @@
 <?php
+include_once $_SERVER['DOCUMENT_ROOT']."/Objects/Repositories/GameRepo.php";
 class GameController {
     
+    public function getAllGames() {
+
+    }
+
+    public function getGame($ID) {
+
+    }
+
+    public function updateGame($ID, $updatedGame) {
+
+    }
+
+    public function toggleVisiblityGame($ID) {
+
+    }
+
+    public function removeGame($ID) {
+
+    }
+
+
+
 }
 ?>

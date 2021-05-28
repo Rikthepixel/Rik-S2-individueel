a3777eb2f2b9d6f7a5146380ea816e1ef0452fe3
RIk <Rikdenbreejen@hotmail.nl>
2021-05-28 10:59:51 +0200

    [Delete] Deleting old code for revision

--- routes/api.php
+++
@@ -1,5 +0,0 @@
-<?php
-$WebsiteRoot = $_SERVER['DOCUMENT_ROOT'];
-
-Router::Get("/Games", [$WebsiteRoot.\Objects\Controllers\GameController::class, 'GetAll']);
-Router::Get("/Games/{:id}/", [$WebsiteRoot.\Objects\Controllers\GameController::class, 'GetSingle']);

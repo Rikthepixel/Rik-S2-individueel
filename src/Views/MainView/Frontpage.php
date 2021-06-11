95a49557f4e01c0087d41ac9be594dc7d7f28579
RIk <Rikdenbreejen@hotmail.nl>
2021-06-12 00:09:26 +0200

    [Change] styling

--- src/Views/MainView/Frontpage.php
+++ src/Views/MainView/Frontpage.php
@@ -8,7 +8,7 @@ include_once $_SERVER["DOCUMENT_ROOT"]."/src/Views/inc/header.html";
             $Project = $Projects[$i];
             
             echo '<div class="project">';
-            echo "<img src='".$Project["project_icon"]."' class='project_image' alt='project'>";
+            echo "<img src='".$Project["project_icon"]."' class='project_image_medium' alt='project'>";
             echo "<a class='project_name' href='/Project.php?id=".$Project['project_info']->id."'>";
             echo $Project["project_info"]->name;
             echo "</a>";

7ff1a56db24d9cfd2dd08e53f30d1b55b1c1e405
RikThePixel <Rikdenbreejen@hotmail.nl>
2021-06-14 11:35:44 +0200

    [Setup] Unittests

---
+++ tests/unit/UnittestFile.php
@@ -0,0 +1,12 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class UnittestFile extends TestCase
+{
+    public function testEmpty(): bool
+    {
+        $this->assertTrue(true);
+
+        return true;
+    }
+}
